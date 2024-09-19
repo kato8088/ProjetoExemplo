81b83508722e9f1567340df7aec47617b1013e93
Kato <kato8088@github.com>
2024-09-19 11:14:43 -0300

    criando as variaveis indefinidas e vazias

--- script1.js
+++ script1.js
@@ -1,9 +1,18 @@
+// declaracao de variavel indefinida
+
+let nome3;
+console.log(nome3);
+
+// declaracao de variavel vazia
+let disciplina = null;
+console.log(disciplina);
+
 // declarando as variáveis
 var nome="fiap";
 console.log(nome);
 
 let nome1="cidade";
-console.log(nome1)
+console.log(nome1);
 
 const nome2="javascript";
-console.log(nome2)
+console.log(nome2);
