be811947863ac5fcb4aa566b6c877e9dc5872dca
Kato <kato8088@github.com>
2024-09-19 11:23:22 -0300

    criando tipos de variaveis

--- script1.js
+++ script1.js
@@ -16,3 +16,25 @@ console.log(nome1);
 
 const nome2="javascript";
 console.log(nome2);
+
+// tipos de variaveis
+
+// inteiro
+let idade = 18;
+console.log(idade)
+
+// float ou com casas decimais
+let valor = 6.40;
+console.log(valor)
+
+// string (com aspas duplas)
+let texto = "fiap";
+console.log(texto)
+
+// boolean
+let aluno = true;
+console.log(aluno)
+
+// array - conjunto de informacoes
+let tecnologias = ["Java", "C++", "HTML", "CSS"];
+console.log(tecnologias)
