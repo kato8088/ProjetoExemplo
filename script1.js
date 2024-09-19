768fbc0dfe2c800c71452bec984a05284f38ce74
Kato <kato8088@github.com>
2024-09-19 11:33:47 -0300

    verificando o tipo da variavel

--- script1.js
+++ script1.js
@@ -17,24 +17,51 @@ console.log(nome1);
 const nome2="javascript";
 console.log(nome2);
 
-// tipos de variaveis
+// TIPOS DE VARIAVEIS
 
 // inteiro
 let idade = 18;
-console.log(idade)
+console.log(idade);
 
 // float ou com casas decimais
 let valor = 6.40;
-console.log(valor)
+console.log(valor);
 
 // string (com aspas duplas)
 let texto = "fiap";
-console.log(texto)
+console.log(texto);
 
 // boolean
 let aluno = true;
-console.log(aluno)
+console.log(aluno);
 
 // array - conjunto de informacoes
 let tecnologias = ["Java", "C++", "HTML", "CSS"];
-console.log(tecnologias)
+console.log(tecnologias);
+
+// CONVERSOES
+
+// conversao de float para int
+
+let numeroFloat = 123.456;
+console.log(numeroFloat);
+console.log(parseInt(numeroFloat));
+
+// conversao de string para float
+
+let numeroString = "987.654";
+console.log(numeroString);
+console.log(parseFloat(numeroString));
+
+// conversao de numero para string
+
+let numeroInt = 789;
+console.log(numeroInt);
+console.log(numeroInt.toString());
+
+// VER O TIPO DA VARIAVEL
+
+let verTipo = typeof numeroInt;
+console.log(verTipo);
+// ou
+console.log(typeof(numeroString));
